f46a7fccbdaefdf86021c610eb5510084a2f5506
Babloo Kumar <34660354+bks1242@users.noreply.github.com>
2023-08-03 12:59:42 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("start --->");
+
+// setTimeout(() => {
+//     console.log("timer expired <---");
+// }, 0);
+
+// // millions of line code.
+// var startTime = new Date().getTime();
+// var endTime = startTime;
+
+// while (endTime < (startTime + 10000)) {
+//     endTime = new Date().getTime();
+// }
+
+console.log("While Expired!!!");
+
+console.log("End!!!");
